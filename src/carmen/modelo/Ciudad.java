1dcd05d1b75c64cc0b55e9353b7dd247e56af22b
juanmabouvier <juanmabouvier@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-06-13 01:06:28 +0000

    estoy modificando un par de cosas de las clases

--- src/carmen/modelo/Ciudad.java
+++ src/carmen/modelo/Ciudad.java
@@ -7,19 +7,30 @@ public class Ciudad {
 	private ArrayList<Local> locales;
 	private Coordenada ubicacion;
 	
-	public Ciudad(Coordenada ubicacion) {
-		ubicacion = ubicacion;
+	//pongo private asi no se puede crear ciudad sin ubicacion
+	private Ciudad() {
 	}
 	
-	public void interrogar(Local local) {
+	public static Ciudad conUbicacion(Coordenada coordenadas) {
+		Ciudad ciudad = new Ciudad();
+		ciudad.ubicacion = coordenadas;
+		return ciudad;
+	}
+	
+	//no tiene sentido que interrogue posta, asi que le pongo de nombre ingresar.
+	//public void interrogar(Local local) {
 		//la ciudad no deberia interrogar, sino el policia (ver como adaptar a mi disenio)
+	//}
+	public char[] ingresar(Local local) {
+		return local.responder();
 	}
 	
 	public void setearLocal(Local local) {
 		this.locales.add(local);
 	}
 	
-	public void setearUbicacion(int ubicacion) {
-		this.ubicacion = ubicacion;
-	}
+	//public void setearUbicacion(int ubicacion) {
+	//	this.ubicacion = ubicacion;
+	//}
+	//creo método de clase y la ciudad se inicia con ubicacion si o si.
 }
--- src/carmen/modelo/ItinerarioDeLadron.java
+++ src/carmen/modelo/ItinerarioDeLadron.java
@@ -7,6 +7,10 @@ public class ItinerarioDeLadron {
 	private Mapa mapa;
 	private ArrayList<Ciudad> ciudades;
 	
+	public ItinerarioDeLadron(Mapa mapa) {
+		this.mapa = mapa;
+	}
+	
 	public void agregarAlRecorrido(Ciudad ciudad) {
 		this.ciudades.add(ciudad);
 	}
