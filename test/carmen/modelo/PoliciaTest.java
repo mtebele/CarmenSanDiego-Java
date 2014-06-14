876c120478050a3fa5722e9aad96bdf535dded89
mati.tebele@gmail.com <mati.tebele@gmail.com@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-06-14 05:57:07 +0000

    - Test commit - Algunos errores de sintaxis corregidos

--- test/carmen/modelo/PoliciaTest.java
+++ test/carmen/modelo/PoliciaTest.java
@@ -1,5 +1,7 @@
 package carmen.modelo;
 
+import java.util.ArrayList;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -10,83 +12,82 @@ public class PoliciaTest {
 
 	@Test
 	public void setUp() {
-		
+
 		Policia policia = new Policia();
-		
+
 	}
-	
+
 	@Test
 	public void policiaDeberiaIniciarComoNovato() {
-	
+
 		Assert.assertEquals(0, policia.getCantArrestos);
-		
+
 		Assert.assertEquals("Novato", policia.verRango.getRando);
 	}
-	
+
 	@Test
 	public void policiaDeberiaAscenderDeRango() {
-				
-		for(int i=0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			policia.realizarArresto();
 		}
 		Assert.assertEquals("Detective", policia.verRango.getRango);
 
-		for(i=0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			policia.realizarArresto();
 		}
 		Assert.assertEquals("Investigador", policia.verRango.getRango);
 
-		for(i=0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			policia.realizarArresto();
 		}
 		Assert.assertEquals("Sargento", policia.verRango.getRango);
 	}
-	
+
 	@Test
 	public void policiaDeberiaTenerDeterminadaVelocidadAlAscenderDeRango() {
-		
+
 		Assert.assertEquals(900, policia.verRango.getVelocidad.getKmPorHora);
-		
-		for(int i=0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			policia.realizarArresto();
 		}
 		Assert.assertEquals(1100, policia.verRango.getVelocidad.getKmPorHora);
 
-		for(i=0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			policia.realizarArresto();
 		}
 		Assert.assertEquals(1300, policia.verRango.getVelocidad.getKmPorHora);
 
-		for(i=0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			policia.realizarArresto();
 		}
 		Assert.assertEquals(1500, policia.verRango.getVelocidad.getKmPorHora);
 	}
-		
+
 	@Test
 	public void policiaDeberiaCambiarDeLugarAlViajar() {
-		
+
 		ArrayList listaDestinos = policia.verDestinos();
 		Ciudad ciudadObjetivo = listaDestinos[0];
-		
+
 		policia.viajar(ciudadObjetivo);
 		Ciudad ciudadActual = policia.ciudadActual();
-		
+
 		Assert.assertEquals(ciudadObjetivo, ciudadActual);
 	}
 
 	@Test
 	public void interrogarDeberiaDevolverLaRespuestaCorrecta() {
-	
+
 		Turno turno = policia.getTurno();
 		Locacion locacion = turno.getLocacion();
 		Ciudad ciudadActual = locacion.ciudadActual();
 		Local local = ciudadActual.getLocalFinanzas();
-	
+
 		local.setearPista("Se fue a Kamchatka");
-	
+
 		Assert.assertEquals(local.responder(), policia.interrogar(local));
 		Assert.assertEquals("Se fue a Kamchatka", local.responder());
-	}	
+	}
 }
-		
