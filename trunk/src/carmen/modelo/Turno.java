60f2e4b18145093d6ca60b2eecca4d3c9c934758
mati.tebele@gmail.com <mati.tebele@gmail.com@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-06-14 05:57:07 +0000

    - Test commit - Algunos errores de sintaxis corregidos

--- trunk/src/carmen/modelo/Turno.java
+++ trunk/src/carmen/modelo/Turno.java
@@ -1,41 +1,44 @@
 package carmen.modelo;
 
+import java.util.ArrayList;
+
 public class Turno {
 
 	private Tiempo tiempo;
 	private Locacion locacion;
-	private ArrayList<Atacable>;
+	private ArrayList<Atacable> listaAtacables;
 
 	public Turno(Locacion locacionInicial) {
 		this.locacion = locacionInicial;
 	}
-	
-	public void actualizar(int horas){
+
+	public void actualizar(int horas) {
 		this.tiempo.transcurrir(horas);
 	}
-	
-	public void viajar(Ciudad destino, Velocidad velocidad){
-		
+
+	public void viajar(Ciudad destino, Velocidad velocidad) {
+
 	}
-	
-	public void interrogar(Local local){
-		
+
+	public void interrogar(Local local) {
+
 	}
-	
-	public boolean quedaTiempo(){
-		// le cambio el nombre al metodo porque quedaba confuso. Devuelve true si queda tiempo.
+
+	public boolean quedaTiempo() {
+		// le cambio el nombre al metodo porque quedaba confuso. Devuelve true
+		// si queda tiempo.
 		return this.tiempo.quedaTiempo();
 	}
-	
-	public Ciudad ciudadActual(){
+
+	public Ciudad ciudadActual() {
 		return null;
 	}
-	
-	public void setearLocacion(Locacion locacion){
+
+	public void setearLocacion(Locacion locacion) {
 		this.locacion = locacion;
 	}
 
-	public int getHorasRestantes(){
+	public int getHorasRestantes() {
 		return 0;
 	}
 }
--- trunk/test/carmen/modelo/TurnoTest.java
+++ trunk/test/carmen/modelo/TurnoTest.java
@@ -10,68 +10,68 @@ public class TurnoTest {
 
 	@Before
 	public void setUp() {
-	
-		//Despues rehago el setUp cuando tengamos mejor definidos los metodos.
-		
+
+		// Despues rehago el setUp cuando tengamos mejor definidos los metodos.
+
 	}
-	
+
 	@Test
 	public void deberianActualizarseBienLasHoras() {
-		
+
 		int HORAS_TOTAL_JUEGO = 154;
-		
+
 		Assert.assertEquals(HORAS_TOTAL_JUEGO, turno.verTiempoRestante);
 		turno.actualizar(3);
-		
+
 		Assert.assertEquals(HORAS_TOTAL_JUEGO - 3, turno.verTiempoRestante);
 	}
-	
+
 	@Test
 	public void deberiaSaberseSiSeAcaboElTiempo() {
-		
+
 		int HORAS_TOTAL_JUEGO = 154;
-		
+
 		Assert.assertEquals(false, turno.seAcaboTiempo());
 		turno.actualizar(HORAS_TOTAL_JUEGO);
-		
+
 		Assert.assertEquals(true, turno.seAcaboTiempo());
 	}
-	
+
 	@Test
 	public void viajarDeberiaCambiarCiudadActual() {
-		
+
 		Velocidad velocidad = new Velocidad();
-		
+
 		Assert.assertEquals(ciudadOrigen, turno.ciudadActual());
 		turno.viajar(ciudad1, velocidad);
-		
+
 		Assert.assertEquals(ciudad1, turno.ciudadActual());
 	}
-	
+
 	@Test
 	public void interrogarDeberiaDevolverRespuestaCorrecta() {
-	
+
 		Assert.assertEquals("Moneda0", turno.interrogar(banco0));
 	}
-	
+
 	@Test
 	public void interrogarDeberiaConsumir1HoraSiNuncaFueVisitado() {
-		
+
 		int HORAS_TOTAL_JUEGO = 154;
-		
+
 		turno.interrogar(banco0);
 		Assert.assertEquals((HORAS_TOTAL_JUEGO - 1), turno.verTiempoRestante());
-		
+
 	}
-	
+
 	@Test
-	public void interrogarDeberiaConsumir2HorasSiFueVisitado2Veces(){
-		
+	public void interrogarDeberiaConsumir2HorasSiFueVisitado2Veces() {
+
 		int HORAS_TOTAL_JUEGO = 154;
-		
+
 		turno.interrogar(banco0);
 		turno.interrogar(banco0);
 		Assert.assertEquals((HORAS_TOTAL_JUEGO - 2), turno.verTiempoRestante());
-		
+
 	}
 }
