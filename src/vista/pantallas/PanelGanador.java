fdffb7d348dda1d7e25901c3f2d7d9e80abf31ad
lucas.lerp@gmail.com <lucas.lerp@gmail.com@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-01 08:23:15 +0000

    Vista de partida, panel de perdedor. Hay que chequear el tema de la redimensión de la pantalla si es posible.

--- src/vista/pantallas/PanelGanador.java
+++ src/vista/pantallas/PanelGanador.java
@@ -6,18 +6,37 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 
 import vista.panelesGenericos.PanelConFondo;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+
 public class PanelGanador extends PanelConFondo{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final String FONDO = "ganador.png";
+	private static final String IMGPATH = "images/";
+	static final String FONDO = IMGPATH + "ganador.png";
 	private BotonMenuPrincipal botonSaltarInicio = new BotonMenuPrincipal();
+	private BotonSalirJuego btnSalirJuego = new BotonSalirJuego();
+	private JLabel mensajeGanaste = new JLabel("FELICITACIONES, HAS RESUELTO EL CASO");
 	
 	public PanelGanador(){
 		super(FONDO);
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.SOUTH, botonSaltarInicio, -60, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, botonSaltarInicio, -450, SpringLayout.EAST, this);
+		setLayout(springLayout);
 		this.add(botonSaltarInicio);
-		this.add(new BotonSalirJuego());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnSalirJuego, -60, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnSalirJuego, -250, SpringLayout.EAST, this);
+		this.add(btnSalirJuego);
+		springLayout.putConstraint(SpringLayout.SOUTH, mensajeGanaste, -400, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, mensajeGanaste, -100, SpringLayout.EAST, this);
+		mensajeGanaste.setFont(new Font("Trebuchet MS", Font.BOLD, 32));
+		mensajeGanaste.setForeground(Color.WHITE);
+		this.add(mensajeGanaste);
 	}
 	
 }
--- src/vista/pantallas/PanelPerdedor.java
+++ src/vista/pantallas/PanelPerdedor.java
@@ -1,14 +1,39 @@
 package vista.pantallas;
+import java.awt.Color;
+import java.awt.Font;
+
 import vista.botonesGenericos.BotonMenuPrincipal;
 import vista.botonesGenericos.BotonSalirJuego;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SpringLayout;
 
 import vista.panelesGenericos.PanelConFondo;
 public class PanelPerdedor extends PanelConFondo {
+	private static final String IMGPATH = "images/";
+	static final String FONDO = IMGPATH + "perdedor.png";
+	private BotonMenuPrincipal botonSaltarInicio = new BotonMenuPrincipal();
+	private BotonSalirJuego btnSalirJuego = new BotonSalirJuego();
+	private JLabel mensajePerdiste = new JLabel("NO HAS PODIDO ATRAPAR AL LADRON");
 	
+
 	public PanelPerdedor() {
+		super(FONDO);
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.SOUTH, botonSaltarInicio, -60, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, botonSaltarInicio, -450, SpringLayout.EAST, this);
+		setLayout(springLayout);
+		this.add(botonSaltarInicio);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnSalirJuego, -60, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnSalirJuego, -250, SpringLayout.EAST, this);
+		this.add(btnSalirJuego);
+		springLayout.putConstraint(SpringLayout.SOUTH, mensajePerdiste, -400, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, mensajePerdiste, -100, SpringLayout.EAST, this);
+		mensajePerdiste.setFont(new Font("Trebuchet MS", Font.BOLD, 32));
+		mensajePerdiste.setForeground(Color.WHITE);
+		this.add(mensajePerdiste);
 		
 	}
 	
