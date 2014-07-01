17c9b330ebe539ee38ee476beacf45ded4fcbc50
lucas.lerp@gmail.com <lucas.lerp@gmail.com@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-01 05:05:40 +0000

    Componentes de algunas vistas sin organizar + nueva clase LabelImagen para las imagenes en los paneles

---
+++ trunk/src/vista/LabelImagen.java
@@ -0,0 +1,33 @@
+package vista;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class LabelImagen extends JLabel{
+	
+	private BufferedImage img;
+	
+	public LabelImagen(String filename){
+		try {
+			img = ImageIO.read(new File(filename));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.setIcon(new ImageIcon(img));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		// pinta el panel con el fondo
+		g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+	}
+
+}
--- trunk/src/vista/pantallas/PanelPartida.java
+++ trunk/src/vista/pantallas/PanelPartida.java
@@ -2,10 +2,53 @@ package vista.pantallas;
 
 import javax.swing.*;
 
+import vista.LabelImagen;
+import vista.botonesGenericos.BotonInfoPolicia;
+import vista.botonesGenericos.BotonInterrogar;
+import vista.botonesGenericos.BotonVolver;
+import vista.panelesGenericos.PanelConFondo;
+
 import java.awt.Color;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelPartida extends PanelConFondo {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static String FONDO = "partida.png";
+	
+	private JTextArea logInvestigacion = new JTextArea();
+	private JTextArea reloj = new JTextArea();
+	private JTextArea diaDeSemana = new JTextArea();
+	private JLabel imagenCiudad = new JLabel();
+	private BotonInterrogar btnInterrogarEconomia = new BotonInterrogar();
+	private BotonInterrogar btnInterrogarCultura = new BotonInterrogar();
+	private BotonInterrogar btnInterrogarTransportes = new BotonInterrogar();
+	private BotonInfoPolicia btnInfoPolicia = new BotonInfoPolicia();
+	private BotonVolver btnVolver = new BotonVolver();
+	private LabelImagen imagenEconomia = new LabelImagen("banco.png");
+	private LabelImagen imagenCultura = new LabelImagen("cultura.png");
+	private LabelImagen imagenTransportes = new LabelImagen("transporte.png");
+	
+	public PanelPartida(){
+		super(FONDO);
+		this.add(reloj);
+		this.add(diaDeSemana);
+		this.add(logInvestigacion);
+		this.add(btnInfoPolicia);
+		this.add(imagenCiudad);
+		this.add(imagenCiudad);
+		this.add(imagenEconomia);
+		this.add(imagenTransportes);
+		this.add(btnInterrogarTransportes);
+		this.add(btnInterrogarEconomia);
+		this.add(btnInterrogarCultura);
+		this.add(btnVolver);
+	}
 	
 }
