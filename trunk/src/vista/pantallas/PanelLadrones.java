4582b3bd2b33f5992acb495cd7119dc6ebc90598
lucas.lerp@gmail.com <lucas.lerp@gmail.com@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-01 20:09:58 +0000

    Cambios en algunas vistas

--- trunk/src/vista/pantallas/PanelLadrones.java
+++ trunk/src/vista/pantallas/PanelLadrones.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 
 import vista.AreaSospechosos;
+import vista.LabelImagen;
 import vista.botonesGenericos.BotonEmitirOrdenArresto;
 import vista.botonesGenericos.BotonVolver;
 import vista.panelesGenericos.PanelConFondo;
@@ -32,5 +33,37 @@ correspodiente a cada ladron particular (PanelPerfilLadron.java).
 */
 
 public class PanelLadrones extends PanelConFondo {
+
+	private static final String IMGPATH = "images/";
+	private static final String IMGPATHFICHAS = "images/fichas/";
 	
+	private static final String FONDO = IMGPATH + "fondo.png";
+	private static final String LADRON1 = IMGPATHFICHAS + "Carmen Sandiego.png";
+	private static final String LADRON2 = IMGPATHFICHAS + "Dazzle Annie.png";
+	private static final String LADRON3 = IMGPATHFICHAS + "Fast Eddie B.png";
+	private static final String LADRON4 = IMGPATHFICHAS + "Ihor Ihorovitch.png";
+	private static final String LADRON5 = IMGPATHFICHAS + "Katherine Drib.png";
+	private static final String LADRON6 = IMGPATHFICHAS + "Lady Agatha.png";
+	private static final String LADRON7 = IMGPATHFICHAS + "Len Bulk.png";
+	private static final String LADRON8 = IMGPATHFICHAS + "Merey Laroc.png";
+	private static final String LADRON9 = IMGPATHFICHAS + "Nick Brunch.png";
+	private static final String LADRON10 = IMGPATHFICHAS + "Scar Graynolt.png";
+	private LabelImagen ladron1 = new LabelImagen(LADRON1);
+	private LabelImagen ladron2 = new LabelImagen(LADRON2);
+	private LabelImagen ladron3 = new LabelImagen(LADRON3);
+	private LabelImagen ladron4 = new LabelImagen(LADRON4);
+	private LabelImagen ladron5 = new LabelImagen(LADRON5);
+	private LabelImagen ladron6 = new LabelImagen(LADRON6);
+	private LabelImagen ladron7 = new LabelImagen(LADRON7);
+	private LabelImagen ladron8 = new LabelImagen(LADRON8);
+	private LabelImagen ladron9 = new LabelImagen(LADRON9);
+	private LabelImagen ladron10 = new LabelImagen(LADRON10);
+	
+	public PanelLadrones(){
+		super(FONDO);
+		setLayout(null);
+		add(ladron1);
+		ladron1.setBounds(10,10,200,300);
+		
+	}
 }
--- trunk/src/vista/pantallas/PanelNuevaPartida.java
+++ trunk/src/vista/pantallas/PanelNuevaPartida.java
@@ -6,6 +6,7 @@ import vista.botonesGenericos.BotonVolver;
 import vista.panelesGenericos.PanelConFondo;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,42 +14,28 @@ public class PanelNuevaPartida extends PanelConFondo {
 	private static final long serialVersionUID = -8273049611334044944L;
 	private static final String IMGPATH = "images/";
 	static final String FONDO = IMGPATH + "newspaper.png";
-	private JLabel introduccion = new JLabel();
-	private JLabel objetoRobado = new JLabel();
-	private JLabel ubicacion = new JLabel();
-	private JLabel deadline = new JLabel();
+	private JTextArea textoReporte = new JTextArea();
 	private JButton btnContinuar = new JButton("Iniciar investigacion");
 	private JButton btnVolver = new BotonVolver();
 
 	public PanelNuevaPartida() {
 		super(FONDO);
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		introduccion.setText("\nEn el dia de la fecha se ha reportado un robo de un objeto importante.\n");
-		introduccion.setForeground(Color.black);
-		introduccion.setHorizontalAlignment(JLabel.CENTER);
-		add(introduccion);
-		objetoRobado.setText("\nSe trata de <objetoRobado>.\n");
-		objetoRobado.setForeground(Color.black);
-		objetoRobado.setHorizontalAlignment(JLabel.CENTER);
-		add(objetoRobado);
-		ubicacion.setText("\nLas mismas se robaron de la ciudad de <nombreCiudad>.\n");
-		ubicacion.setForeground(Color.black);
-		add(ubicacion);
-		deadline.setText("\nTiene una semana para resolver el caso.\n");
-		deadline.setForeground(Color.black);
-		add(deadline);
+		setLayout(null);
+		textoReporte.setOpaque(false);
+		textoReporte.setEditable(false);
+		textoReporte.setText
+		("\nEn el dia de la fecha se ha reportado un robo de un objeto importante."
+				+ "\nSe trata de <objetoRobado>."+
+				"\nEl mismo se reporto en la ciudad de <nombreCiudad>."+
+				"\nTiene una semana para resolver el caso.\n");
+		add(textoReporte);
+		textoReporte.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+		textoReporte.setBounds(200, 200, 500, 500);
 		add(btnContinuar);
+		btnContinuar.setBounds(175, 400, 200, 40);
 		add(btnVolver);
-		setVisible(true);
-		btnContinuar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFrame pepe = new JFrame();
-				pepe.setSize(400, 400);
-				pepe.setVisible(true);
-				pepe.add(new PanelOrdenArresto(IMGPATH + "ordenarresto.jpg"));
-			}
-		});
-
+		btnVolver.setBounds(425, 400, 200, 40);
+		
 	}
 
 }
