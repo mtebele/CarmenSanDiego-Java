9f6ccb39ffa8e31fd7863244c5f557b43d25a603
alejandro.alazraqui <alejandro.alazraqui@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-01 08:13:29 +0000

    Panelviajar completado

--- trunk/src/vista/pantallas/PanelViajar.java
+++ trunk/src/vista/pantallas/PanelViajar.java
@@ -1,6 +1,7 @@
 package vista.pantallas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
@@ -28,8 +29,24 @@ import modelo.ladron.perfil.Vehiculo;
 
 public class PanelViajar extends PanelConFondo{
 	
-	private static final String FONDO = "viaje.png";
-	private LabelImagen mapa = new LabelImagen("mapamundi.png");
+	private static final String IMGPATH = "images/";
+	static final String FONDO = IMGPATH + "mapa.jpg";
+	static final int TAMLETRA=20;
+	static final int ANCHO= 80;
+	static final int ALTO=125;
+	static final int YDESTINO=450;
+	static final int XDESTINO1=50;
+	static final int XDESTINO2=250;
+	static final int XDESTINO3=450;
+	static final int XDESTINO4=650;
+	static final int TAMVIAJAR=80;
+	static final int XVIAJAR=375;
+	static final int YVIAJAR=150;
+	static final int XBOTON=700;
+	static final int YBOTON=0;
+	static final int TAMVOLVER=100;
+	static final int XVOLVER=700;
+	static final int YVOLVER=0;
 	private JLabel lblViajarA = new JLabel("Viajar a:");
 	private JButton btnDestino1 = new JButton("<destino1>");
 	private JButton btnDestino2 = new JButton("<destino2>");
@@ -39,7 +56,15 @@ public class PanelViajar extends PanelConFondo{
 	
 	public PanelViajar(){
 		super(FONDO);
-		add(mapa);
+		setLayout(null);
+		lblViajarA.setBounds(XVIAJAR,YVIAJAR,TAMVIAJAR,TAMVIAJAR);
+		lblViajarA.setForeground(Color.black);
+		lblViajarA.setFont(new java.awt.Font("Tahoma", 0, TAMLETRA)); 
+		btnDestino1.setBounds(XDESTINO1,YDESTINO,ALTO,ANCHO);
+		btnDestino2.setBounds(XDESTINO2,YDESTINO,ALTO,ANCHO);
+		btnDestino3.setBounds(XDESTINO3,YDESTINO,ALTO,ANCHO);
+		btnDestino4.setBounds(XDESTINO4,YDESTINO,ALTO,ANCHO);
+		btnVolver.setBounds(XVOLVER,YVOLVER,TAMVOLVER,TAMVOLVER);
 		add(lblViajarA);
 		add(btnDestino1);
 		add(btnDestino2);
