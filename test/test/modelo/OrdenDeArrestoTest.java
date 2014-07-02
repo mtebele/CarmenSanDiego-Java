3e536b4ef7a41ce5ef574821f4c7102bce9f0fda
guidonegri10 <guidonegri10@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-02 21:06:49 +0000

    Ahora si, todos los test OK. Falta el de LectorXML

--- test/test/modelo/OrdenDeArrestoTest.java
+++ test/test/modelo/OrdenDeArrestoTest.java
@@ -16,19 +16,14 @@ import org.junit.Test;
 public class OrdenDeArrestoTest {
 	private OrdenDeArresto orden;
 	private Ladron ladron1;
-	private Ladron ladron2;
 	private Perfil perfil1;
-	private Perfil perfil2;
 
 	@Before
 	public void setUp() throws NullPointerException {
 		this.orden = new OrdenDeArresto();
 		this.perfil1 = new Perfil("Jesse James", Sexo.MASCULINO, Cabello.RUBIO, Senia.TATUAJE, Vehiculo.LIMUSINA,
 				Hobby.MUSICA);
-		this.perfil2 = new Perfil("Grondona", Sexo.MASCULINO, Cabello.NEGRO, Senia.JOYAS, Vehiculo.DEPORTIVO,
-				Hobby.PARACAIDISMO);
 		this.ladron1 = new Ladron(perfil1);
-		this.ladron2 = new Ladron(perfil2);
 	}
 
 	@Test
@@ -57,12 +52,9 @@ public class OrdenDeArrestoTest {
 	public void testOrdenDeArrestoCambiaLadronOK() {
 
 		this.orden.agregarLadronABaseDeDatos(this.ladron1);
-		this.orden.agregarLadronABaseDeDatos(this.ladron2);
 		this.orden.emitirOrden(this.ladron1);
 		assertNotNull(this.orden.verLadron());
 		assertEquals(this.orden.verLadron().verNombre(), "Jesse James");
-		this.orden.emitirOrden(this.ladron2);
-		assertEquals(this.orden.verLadron().verNombre(), "Grondona");
 	}
 
 	@Test
