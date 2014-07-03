e1efd673867751d83bcadfcb9199f788a98283ad
alejandro.alazraqui <alejandro.alazraqui@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-03 22:18:28 +0000

    Panel de carga borrado

--- src/vista/pantallas/JuegoVista.java
+++ src/vista/pantallas/JuegoVista.java
@@ -4,14 +4,13 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
-import vista.panelesGenericos.PanelCarga;
 
 public class JuegoVista extends JFrame {
 	private static final long serialVersionUID = -3021501308427221124L;
 	private static int MAX_ALTURA = 800;
 	private static int MAX_ANCHURA = 600;
 
-	PanelCarga carga = new PanelCarga();
+	
 	PanelInicial pnlInicial = new PanelInicial();
 
 	public JuegoVista() {
