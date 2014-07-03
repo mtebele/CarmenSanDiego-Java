4da79bd655d7785e6ad0f8ca9cdde2234d861c0c
guidonegri10 <guidonegri10@47ade9e0-9fc7-ee50-c159-daa77c7da90c>
2014-07-03 20:44:46 +0000

    Eliminado código que había quedado comentado.

--- trunk/test/test/modelo/ObjetoRobadoTest.java
+++ trunk/test/test/modelo/ObjetoRobadoTest.java
@@ -3,8 +3,6 @@ package test.modelo;
 import junit.framework.Assert;
 import modelo.ladron.ObjetoRobado;
 import modelo.ladron.Valor;
-import modelo.mapa.Ciudad;
-import modelo.mapa.Coordenada;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,12 +10,10 @@ import org.junit.Test;
 public class ObjetoRobadoTest {
 
 	private ObjetoRobado objeto;
-	private Ciudad ciudad;
 	private Valor valor;
 
 	@Before
 	public void setUp() {
-		this.ciudad = new Ciudad(new Coordenada(10, 10));
 		this.valor = Valor.COMUN;
 		this.objeto = new ObjetoRobado(Valor.COMUN);
 	}
